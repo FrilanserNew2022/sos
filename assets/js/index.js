a55ad28ff15be2e348679078f781a64ce185d4f0
Поташов Олег <potashov_2004@mail.ru>
2023-10-13 01:34:21 +0500

    mate to main images scroll and fix bugs in header

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,5 @@
 
 let animationLang = false;
-let menuOpen = false;
 document.querySelector('.header__lang-box').addEventListener('click', () => {
     if(!animationLang){
         document.querySelector('.header__lang-box').classList.toggle('open')
@@ -10,6 +9,8 @@ document.querySelector('.header__lang-box').addEventListener('click', () => {
         animationLang = false
     }, 500)
 });
+
+let menuOpen = false;
 
 document.querySelector('.menu__burger').addEventListener('click', () => {
     if(!menuOpen){
@@ -22,4 +23,28 @@ document.querySelector('.menu__burger').addEventListener('click', () => {
     setTimeout(() => {
         menuOpen = false
     }, 1000)
+})
+
+let currentImg = 0;
+
+function positionMainImg() {
+    const collectionContainer = document.querySelectorAll('.img__big');
+    collectionContainer.forEach((el, index) => {
+        const position = (index - currentImg) * 100
+        el.style.transform = `translate(${position}%, 0)`
+    })
+}
+
+positionMainImg();
+
+document.querySelector('.main__button-prev').addEventListener('click', () => {
+    if (currentImg === 0) return;
+    currentImg = currentImg - 1;
+    positionMainImg();
+})
+
+document.querySelector('.main__button-next').addEventListener('click', () => {
+    if (currentImg === 3) return;
+    currentImg = currentImg + 1;
+    positionMainImg();
 })
