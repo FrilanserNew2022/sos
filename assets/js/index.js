f6c42ae4a8ddbc9a0d59c54b1d498d601b8000c8
Поташов Олег <potashov_2004@mail.ru>
2023-10-15 21:20:40 +0500

    make a adaptive royal block and fix bug in main block when decreasing screen

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,14 +28,13 @@ document.querySelector('.menu__burger').addEventListener('click', () => {
 let currentImg = 0;
 
 function positionMainImg() {
+    document.querySelector('.circles__full').style.transform = `translate(${currentImg * 22}px, 0)`
     const collectionContainer = document.querySelectorAll('.img__big');
     collectionContainer.forEach((el, index) => {
         const position = (index - currentImg) * 100
         el.style.transform = `translate(${position}%, 0)`
     })
 }
-
-positionMainImg();
 
 document.querySelector('.main__button-prev').addEventListener('click', () => {
     if (currentImg === 0) return;
@@ -47,4 +46,8 @@ document.querySelector('.main__button-next').addEventListener('click', () => {
     if (currentImg === 3) return;
     currentImg = currentImg + 1;
     positionMainImg();
+})
+
+document.querySelector('.current').addEventListener('', () => {
+    console.log(1)
 })
